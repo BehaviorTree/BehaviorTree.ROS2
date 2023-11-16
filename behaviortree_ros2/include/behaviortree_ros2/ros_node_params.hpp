b89dc196865cb6e76a332afbcd99b4379cbd0ab6
Davide Faconti <davide.faconti@gmail.com>
2023-11-16 12:28:07 +0100

    add comment

--- behaviortree_ros2/include/behaviortree_ros2/ros_node_params.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/ros_node_params.hpp
@@ -35,7 +35,10 @@ struct RosNodeParams
   std::string default_port_value;
 
   // parameters used only by service client and action clients
+
+  // timeout when sending a request
   std::chrono::milliseconds server_timeout = std::chrono::milliseconds(1000);
+  // timeout used when detecting the server the first time
   std::chrono::milliseconds wait_for_server_timeout = std::chrono::milliseconds(500);
 };
 

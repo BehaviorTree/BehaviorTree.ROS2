f1fad000274a293ae490a86656f67dbad12a59ac
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-10-01 18:15:05 +0300

    Deprecate rmw_qos_profile_t
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>

--- behaviortree_ros2/include/behaviortree_ros2/bt_service_node.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/bt_service_node.hpp
@@ -19,6 +19,7 @@
 #include <string>
 #include <rclcpp/executors.hpp>
 #include <rclcpp/allocator/allocator_common.hpp>
+#include <rclcpp/qos.hpp>
 #include "behaviortree_cpp/bt_factory.h"
 
 #include "behaviortree_ros2/ros_node_params.hpp"
@@ -216,7 +217,7 @@ inline RosServiceNode<T>::ServiceClientInstance::ServiceClientInstance(
       node->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive, false);
   callback_executor.add_callback_group(callback_group, node->get_node_base_interface());
 
-  service_client = node->create_client<T>(service_name, rmw_qos_profile_services_default,
+  service_client = node->create_client<T>(service_name, rclcpp::ServicesQoS(),
                                           callback_group);
 }
 

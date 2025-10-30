1ab924d7b19e6dc26c3eb1cd5afcc1ac0a54a713
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-10-30 07:56:25 +0300

    Deprecated rmw_qos_profile_t and ament_target_dependencies (#119)
    * Deprecated ament_target_dependencies
    
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
    
    * Deprecate rmw_qos_profile_t
    
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
    
    * Keep rmw_qos_profile_t for humble
    
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
    
    ---------
    
    Signed-off-by: ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>

--- behaviortree_ros2/include/behaviortree_ros2/bt_service_node.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/bt_service_node.hpp
@@ -19,6 +19,8 @@
 #include <string>
 #include <rclcpp/executors.hpp>
 #include <rclcpp/allocator/allocator_common.hpp>
+#include <rclcpp/version.h>
+#include <rclcpp/qos.hpp>
 #include "behaviortree_cpp/bt_factory.h"
 
 #include "behaviortree_ros2/ros_node_params.hpp"
@@ -216,8 +218,14 @@ inline RosServiceNode<T>::ServiceClientInstance::ServiceClientInstance(
       node->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive, false);
   callback_executor.add_callback_group(callback_group, node->get_node_base_interface());
 
+  // For Jazzy and Later Support
+  #if RCLCPP_VERSION_GTE(28, 0, 0)
+  service_client = node->create_client<T>(service_name, rclcpp::ServicesQoS(),
+                                          callback_group);
+  #else
   service_client = node->create_client<T>(service_name, rmw_qos_profile_services_default,
                                           callback_group);
+  #endif
 }
 
 template <class T>

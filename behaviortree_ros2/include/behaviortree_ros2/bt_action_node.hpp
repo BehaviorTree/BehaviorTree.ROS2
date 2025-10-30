0cc2da20518ab47c26ae2c742bed04904ce7cfe0
pepeRossRobotics <66112254+pepeRossRobotics@users.noreply.github.com>
2025-10-30 05:16:51 +0000

    Update bt_action_node.hpp (#89)
    Fix to avoid crashing the BT

--- behaviortree_ros2/include/behaviortree_ros2/bt_action_node.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/bt_action_node.hpp
@@ -426,6 +426,7 @@ inline NodeStatus RosActionNode<T>::tick()
           if(!goal_handle_)
           {
             RCLCPP_ERROR(logger(), "Goal was rejected by server");
+            return onFailure(GOAL_REJECTED_BY_SERVER);
           }
           else
           {
